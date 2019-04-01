98e99e5b0d250fdafd7ec9143e7933bded9417fd
Finn <Bromenschenkel>
2019-04-01 15:15:02 -0700

    Added Die() and Monster()

---
+++ Assets/Scenes/Die.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class Die : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public int Roll(int times)
+    {
+        int total = 0;
+        for (int i = 0; i < times; i ++)
+        {
+            total += Random.Range(1, 6);
+        }
+
+        return total;
+    }
+}
---
+++ Assets/Scenes/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Monster : MonoBehaviour
+{
+    int[] dc = new int[6]; //all monster cards inherit from this
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected abstract bool Fight(int roll, int character);
+}
