44a1e5a0c913a28d844b17294f866205ad8fd6f4
Finn <Bromenschenkel>
2019-04-03 15:35:41 -0700

    Added Player.cs

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int playerClass; //changed by class
+    int win;
+    int Win
+    {
+        set
+        {
+            if (value == 3)
+            {
+                win = 2000;
+            }
+            else if (value == 4)
+            {
+                win = 3000;
+            }
+            else 
+            {
+                win = 1000;
+            }
+        }
+        get
+        {
+            return win;
+        }
+    } //ditto
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetClass(int tempClass)
+    {
+        playerClass = tempClass;
+        Win = tempClass;
+    }
+}
