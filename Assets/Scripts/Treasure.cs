0d90aa2048250d99e6dd487cc81e58c6b899554f
Finn <Bromenschenkel>
2019-05-29 15:06:49 -0700

    Refactored Treasure.cs

--- Assets/Scripts/Treasure.cs
+++ Assets/Scripts/Treasure.cs
@@ -4,19 +4,5 @@ using UnityEngine;
 
 public class Treasure : MonoBehaviour
 {
-
-    public int value =50;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //int a = 3;
-    }
-
+    public int value = 50;
 }
