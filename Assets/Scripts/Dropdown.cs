9caf0e1b6c22c1cd8562659f7766f415a1a35b5c
Finn <Bromenschenkel>
2019-06-04 14:07:38 -0700

    Added janky dropdown script aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Assets/Scripts/Dropdown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dropdown : MonoBehaviour
+{
+    public Dropdown dropdown;
+    private Text selection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dropdown = GetComponent<Dropdown>();
+        dropdown.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
